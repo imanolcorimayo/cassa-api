21def5f002da50d252d98a613b69d5c3b0271058
imanolcorimayo <imanolcorimayo@gmail.com>
2022-04-16 17:35:46 -0300

    update Product route added

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -48,5 +48,38 @@ module.exports = {
     deleteProduct: async (req, res) => {
     },
     updateProduct: async (req, res) => {
+        const {
+            id,
+            name,
+            category,
+            buyPrice,
+            sellPrice,
+            quantity,
+            buyUnit,
+            sellUnit,
+        }=req.body;
+
+        try {
+            const product = await Product.findOne({
+                where: { id },
+            })
+            if (product) {
+                product.name = name ? name : product.name;
+                product.type = category ? category : product.type;
+                product.buy_price= buyPrice ? buyPrice : product.buy_price;
+                product.sell_price= sellPrice ? sellPrice : product.sell_price;
+                product.quantity= quantity ? quantity: product.quantity;
+                product.buy_unit= buyUnit ? buyUnit : product.buy_unit;
+                product.sell_unit= sellUnit ? sellUnit : product.sell_unit;
+                product.save()
+                res.status(200).send('Product was updated!')
+            } else {
+                console.log(product)
+                res.status(501).send("Something wrong happens with db")
+            }
+        } catch (error) {
+            console.error(error)
+            res.status(501).send(error)
+        }
     },
 }
